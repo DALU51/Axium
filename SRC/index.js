bc621f4c4406c5799a78118e986d6e9fc6978db5
DALU51 <62446212+DALU51@users.noreply.github.com>
2025-05-18 01:50:19 -0400

    no test

--- SRC/index.js
+++ SRC/index.js
@@ -1,19 +1,6 @@
 import express from "express";
 const router = express();
-import db from "./Routes/database.js"
 
-router.get('/test',(req,res) => {
-
-    db.query('SELECT * FROM ACCOUNTS', function(err,rows,fields){
-            if (err){
-            console.log(err)
-        } else {
-            res.json(rows)
-            console.log("Accounts sent")
-        }
-    })
- 
-})
 
 
 router.get('/',(req,res) => {
